a4a63f4e255952f92eeeea4a28ede082f9c8ab2f
Mohamed Elsayed Sadik <168742130+MohamedSadik7@users.noreply.github.com>
2025-01-07 15:53:38 +0200

    Add files via upload

---
+++ fapp.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+import plotly.express as px
+
+st.set page.config(
+    layout="wide"
+page_title= "simple dashboard"
+    
+)
+
+df-px.data.tips()
+
+#sidebar
+
+#x =st.sidebar.checkbox ("Show data", True, key=1)
+#ay= st.sidebar.selectbox("Select day", df.['days'].unique())
